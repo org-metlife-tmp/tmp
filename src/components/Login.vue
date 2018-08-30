ba49142ec587110552cb34a360433805bea8299c
songsl <123@qq.com>
2018-08-30 16:47:26 +0800

    更换logo

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -188,7 +188,8 @@
                 </div>
             </div>
         </div>
-        <div class="footer-text">Copyright &copy; 2009光大永明人寿保险有限公司 津ICP备05001011</div>
+        <!--Copyright &copy; 2009光大永明人寿保险有限公司 津ICP备05001011-->
+        <div class="footer-text"></div>
     </div>
 </template>
 
