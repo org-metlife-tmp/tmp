18c303ec13d1604de3ada7254c87a6b8daa0a355
xiaoyan <nayinianxy@163.com>
2018-09-27 10:27:56 +0800

    电子回单

--- src/components/electronicReceipt/ElectronicReceipt.vue
+++ src/components/electronicReceipt/ElectronicReceipt.vue
@@ -8,13 +8,23 @@
         /*搜索区*/
         .search-setion {
             text-align: left;
+            height: 42px;
+            overflow: hidden;
+            transition: height 1s;
             .line {
                 text-align: center;
             }
         }
-        
+        .search-setion.show-more {
+            height: 184px;
+        }
+
         .table-content{
-            height: 325px;
+            height: 339px;
+            transition: height 1s;
+        }
+        .is-small {
+            height: 40%;
         }
 
         /*分隔栏*/
@@ -41,34 +51,85 @@
             height: 8%;
             bottom: -10px;
         }
+        /*顶部下拉部分*/
+        .btn-list-left{
+            position: absolute;
+            top: -58px;
+            left: -20px;
+        }
+    }
+</style>
+<style lang="less" type="text/less">
+    #electronicReceipt{
+        .el-form-item--mini.el-form-item, .el-form-item--small.el-form-item{
+            width: 90%;
+        }
+        .search-setion {
+            .el-form {
+                .el-form-item__content {
+                    width: 100%;
+                }
+            }
+            .el-select {
+                width: 90%;
+            }
+        }
+        .el-dialog__wrapper {
+            .el-dialog__body {
+                height: 440px;
+                overflow-y: auto;
+            }
+        }
     }
 </style>
 
 <template>
     <div id="electronicReceipt">
+        <div class="btn-list-left">
+            <el-select v-model="channel_code" 
+                        placeholder="渠道编码" size="mini"
+                        clearable>
+                <el-option v-for="channel in channelList"
+                            :key="channel.channel_code"
+                            :label="channel.channel_code"
+                            :value="channel.channel_code">
+                </el-option>
+            </el-select>
+            <el-select v-model="ebObj"
+                        :disabled="!channel_code"
+                        placeholder="银行" size="mini"
+                        @change="bankIsSelect"
+                        value-key="eb_type">
+                <el-option v-for="type in bankList"
+                            :key="type.eb_type"
+                            :label="type.eb_type_desc"
+                            :value="type">
+                </el-option>
+            </el-select>
+        </div>
         <!--搜索区-->
-        <div class="search-setion">
+        <div :class="['search-setion',{'show-more':showMore}]">
             <el-form :inline="true" :model="searchData" size="mini">
                 <el-row>
-                    <el-col :span="4">
-                        <el-form-item>
-                            <el-input v-model="searchData.pay_query_key" clearable placeholder="请输入付款方名称或账号"></el-input>
-                        </el-form-item>
-                    </el-col>
-                    <el-col :span="4">
-                        <el-form-item>
-                            <el-input v-model="searchData.recv_query_key" clearable
-                                      placeholder="请输入收款方名称或账号"></el-input>
-                        </el-form-item>
-                    </el-col>
                     <el-col :span="6">
                         <el-form-item>
+                            <el-date-picker
+                                    v-model="searchData.eb_date"
+                                    type="date"
+                                    placeholder=" 回单日期"
+                                    value-format="yyyy-MM-dd"
+                                    style="width: 100%;">
+                            </el-date-picker>
+                        </el-form-item>
+                    </el-col>
+                    <el-col :span="8">
+                        <el-form-item>
                             <el-col :span="11">
-                                <el-input v-model="searchData.min" clearable placeholder="最小金额"></el-input>
+                                <el-input v-model="searchData.min" clearable placeholder="最小回单金额"></el-input>
                             </el-col>
                             <el-col class="line" :span="2">-</el-col>
                             <el-col :span="11">
-                                <el-input v-model="searchData.max" clearable placeholder="最大金额"></el-input>
+                                <el-input v-model="searchData.max" clearable placeholder="最大回单金额"></el-input>
                             </el-col>
                         </el-form-item>
                     </el-col>
@@ -77,33 +138,87 @@
                             <el-button type="primary" plain @click="queryData" size="mini">搜索</el-button>
                         </el-form-item>
                     </el-col>
+                    <el-col :span="3" :offset="5">
+                        <el-button type="primary" plain @click="showMore = !showMore" size="mini">
+                            高级<i
+                                :class="['el-icon--right',{'el-icon-arrow-down':!showMore},{'el-icon-arrow-right':showMore}]"></i>
+                        </el-button>
+                    </el-col>
+                </el-row>
+                <el-row>
+                    <el-col :span="6">
+                        <el-select v-model="searchData.addKey"
+                                    placeholder="增加搜索条件" size="mini"
+                                    @change="addSearch"
+                                    value-key="key">
+                            <el-option v-for="type in searchOptionList"
+                                        :key="type.key"
+                                        :label="type.value"
+                                        :value="type">
+                            </el-option>
+                        </el-select>
+                    </el-col>
+                    <template v-for="item in searchList">
+                        <el-col :span="6" :key="item.key">
+                            <el-form-item>
+                                <el-input v-model="searchData[item.key]" clearable :placeholder="item.desc" class="input-with-select">
+                                    <el-button slot="append"  type="primary" @click="delSearch(item)">删除</el-button>
+                                </el-input>
+                            </el-form-item>
+                        </el-col>
+                    </template>
                 </el-row>
             </el-form>
         </div>
         <!--分隔栏-->
         <div class="split-bar"></div>
         <!--数据展示区-->
-        <section class="table-content">
+        <section :class="['table-content',{'is-small':showMore}]">
             <el-table :data="tableList"
                       height="100%"
                       border size="mini">
-                <el-table-column prop="batchno" label="本方账户" :show-overflow-tooltip="true"></el-table-column>
-                <el-table-column prop="total_num" label="交易日期" :show-overflow-tooltip="true"></el-table-column>
-                <el-table-column prop="total_amount" label="方向" :show-overflow-tooltip="true"></el-table-column>
-                <el-table-column prop="success_num" label="对方户名" :show-overflow-tooltip="true"></el-table-column>
-                <el-table-column prop="success_num" label="对方账户号" :show-overflow-tooltip="true"></el-table-column>
-                <el-table-column prop="success_amount" label="金额" :show-overflow-tooltip="true"
-                                 :formatter="transitAmount"></el-table-column>
-                <el-table-column prop="service_status" label="摘要" :show-overflow-tooltip="true"></el-table-column>
                 <el-table-column
-                        label="操作" width="110"
+                    prop="eb_date"
+                    label="回单日期"
+                    :show-overflow-tooltip="true"
+                >
+                </el-table-column>
+                <el-table-column
+                    prop="amount"
+                    label="回单金额"
+                    :formatter="changeThousandth"
+                    :show-overflow-tooltip="true"
+                >
+                </el-table-column>
+                <template v-for="head in tableHead">
+                    <!-- 业务种类列，需要格式化数据-->
+                    <el-table-column
+                        v-if="head.prop=='biz_type' || head.prop=='service_status' || head.prop=='interactive_mode'"
+                        :key="head.id"
+                        :prop="head.prop"
+                        :label="head.name"
+                        :formatter="transitionStatus"
+                        :show-overflow-tooltip="true"
+                    >
+                    </el-table-column>
+                    <!-- 公用列 -->
+                    <el-table-column
+                        v-else
+                        :key="head.prop"
+                        :prop="head.prop"
+                        :label="head.origin_fd_desc"
+                        :show-overflow-tooltip="true"
+                    >
+                    </el-table-column>
+                </template>
+                <el-table-column
+                        label="操作" width="80"
                         fixed="right">
                     <template slot-scope="scope" class="operationBtn">
                         <el-tooltip content="查看" placement="bottom" effect="light"
-                                    :enterable="false" :open-delay="500"
-                                    v-if="scope.row.service_status != 1 && scope.row.service_status != 5">
+                                    :enterable="false" :open-delay="500">
                             <el-button type="primary" icon="el-icon-search" size="mini"
-                                       @click="lookBill(scope.row)"></el-button>
+                                    @click="viewDetail(scope.row,scope.$index)"></el-button>
                         </el-tooltip>
                     </template>
                 </el-table-column>
@@ -123,12 +238,12 @@
                     :current-page="pagCurrent">
             </el-pagination>
         </div>
-        <!--待处理编辑弹出框-->
+        <!--查看弹出框-->
         <el-dialog :visible.sync="dialogVisible"
-                   width="800px" title="调拨单信息查看 "
+                   width="800px" title="收款回单查看"
                    :close-on-click-modal="false"
                    top="56px">
-            <h1 slot="title" class="dialog-title">调拨单信息查看</h1>
+            <h1 slot="title" class="dialog-title">收款回单查看</h1>
         </el-dialog>
     </div>
 </template>
@@ -138,18 +253,27 @@
         name: "ElectronicReceipt",
         created: function(){
             this.$emit("transmitTitle", "电子回单");
-            this.$emit("getCommTable", this.routerMessage);
+            // this.$emit("getCommTable", this.routerMessage);
         },
         mounted:function(){
+            this.channelList = [{
+                channel_code: 'cmbc'
+            }];
+            var typeList = JSON.parse(window.sessionStorage.getItem("eleType"));
+            if(typeList){
+                this.bankList = typeList;
+            }
         },
         props: ["tableData"],
         data: function () {
             return {
                 routerMessage: {
-                    optype: "wftrans_list",
+                    optype: "ele_list",
                     params: {
                         page_size: 8,
-                        page_num: 1
+                        page_num: 1,
+                        channel_code: "",
+                        eb_type: ""
                     }
                 },
                 tableList: [],//列表数据
@@ -158,12 +282,15 @@
                 pagCurrent: 1,
                 dialogVisible: false,
                 dialogData: {},
-                searchData: { //搜索条件
-                    pay_query_key: "",
-                    recv_query_key: "",
-                    min: "",
-                    max: "",
-                },
+                searchData: {}, //搜索条件
+                showMore: false,
+                searchOptionList: [],//可供增加的搜索条件的下拉列表
+                searchList: [],//增加的搜索条件
+                channelList: [],//渠道编码
+                bankList: [],//银行
+                channel_code: "",//当前渠道编码
+                ebObj: {},//当前银行
+                tableHead: [], //动态表格的列名称
             }
         },
         methods: {
@@ -182,14 +309,82 @@
             },
             //根据条件查询
             queryData:function (){
-
+                var searchData = this.searchData;
+                debugger
+                Object.assign(this.routerMessage.params,searchData);
+                this.$emit("getCommTable", this.routerMessage);
             },
-            //展示格式转换-金额
-            transitAmount: function (row, column, cellValue, index) {
-                return this.$common.transitSeparator(cellValue);
+            //增加搜索条件
+            addSearch:function (val){
+                this.searchList.push({key: val.key, desc: val.value});
             },
-            //查看
-            lookBill: function (row) {
+            delSearch: function (item){
+                var index = this.searchList.indexOf(item)
+                this.searchList.splice(index, 1);
+                this.searchData[item.key] = "";
+                if (this.searchList.length === 0) {
+                    this.searchData.addKey = {};
+                }
+            },
+            //处理表格千分位
+            changeThousandth: function (row, column, cellValue, index) {
+                if(cellValue){
+                    return this.$common.transitSeparator(cellValue);
+                }
+            },
+            //请求模板和列表
+            bankIsSelect: function (val){
+                //查询电子回单模板
+                this.$axios({
+                    url: "/cfm/normalProcess",
+                    method: "post",
+                    data: {
+                        optype: "ele_template",
+                        params: {
+                            uuid: val.uuid
+                        }
+                    }
+                }).then((result) => {
+                    if (result.data.error_msg) {
+                        this.$message({
+                            type: "error",
+                            message: result.data.error_msg,
+                            duration: 2000
+                        })
+                    } else {
+                        var uuid = this.ebObj.uuid;
+                        var data = result.data.data[uuid];
+                        var tabHeadList = [];
+                        var sOptionlist = [];
+                        if(JSON.stringify(data) == "{}"){
+                            this.tableHead = [];
+                            this.searchOptionList = [];
+                            this.tableList = [];
+                            return;
+                        }
+                        for(let i = 1;i<7; i++){
+                            var obj = {};
+                            obj.key = 'field_' + i;
+                            var item = data['field_'+i];
+                            item.prop = 'field_' + i;
+                            obj.value = item.origin_fd_desc;
+                            tabHeadList.push(item);
+                            sOptionlist.push(obj);
+                        }
+                        //组织表格列名称数据
+                        this.tableHead = tabHeadList;
+                        //组织搜索条件下拉数据
+                        this.searchOptionList = sOptionlist;
+                        debugger
+                        //查询电子回单列表
+                        this.routerMessage.params.channel_code = this.channel_code;
+                        this.routerMessage.params.eb_type = val.eb_type;
+                        this.$emit("getCommTable", this.routerMessage);
+                    }
+                })
+            },
+            //查看详情
+            viewDetail: function(row, index){
                 this.dialogVisible = true;
             }
         },
