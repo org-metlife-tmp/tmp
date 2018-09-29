2d90be2c82adca56f8d1385e9e80b99d806da320
hongda <hongda@qihangjinfu.com>
2018-09-29 15:02:55 +0800

    余额通，交易通添加默认查询时间

--- src/components/tradeLinks/HistoryDealDetail.vue
+++ src/components/tradeLinks/HistoryDealDetail.vue
@@ -114,15 +114,20 @@
         name: "HistoryDealDetail",
         created: function () {
             let curDate = new Date();
+	    let yesterDay = new Date();
             let oldDate = new Date();
+	    yesterDay.setFullYear(curDate.getFullYear());
+	    yesterDay.setMonth(curDate.getMonth());
+	    yesterDay.setDate(curDate.getDate()-1);
             oldDate.setFullYear(curDate.getFullYear());
             oldDate.setMonth(curDate.getMonth());
-            oldDate.setDate(curDate.getDate()-7);
+            oldDate.setDate(curDate.getDate()-8);
 
-            this.dateValue = [oldDate,curDate];
+            this.dateValue = [oldDate,yesterDay];
 
             this.routerMessage.params.start_date = this.dateValue[0];
             this.routerMessage.params.end_date = this.dateValue[1];
+
             //向父组件发送自己的信息
             this.$emit('transmitTitle', '历史交易明细');
             this.$emit('getTableData', this.routerMessage);
