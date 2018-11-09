b7cf4066a243220c91d5db9cb989547f16fe213f
hongda <hongda@qihangjinfu.com>
2018-11-09 11:59:06 +0800

    线下补录(自动扣款)付款方账户可以是直连，也能是非直连

--- src/components/payment/PayMakeBill.vue
+++ src/components/payment/PayMakeBill.vue
@@ -671,7 +671,7 @@
                             optype:"account_normallist",
                             params:{
                                 status:1,
-                                interactive_mode: pay_mode ? (pay_mode == "1" ? "1" : "2") : ""
+                                interactive_mode: pay_mode !=8  ? (pay_mode == "1" ? "1" : "2") : ""
                             }
                         }
                     }).then((result) =>{
