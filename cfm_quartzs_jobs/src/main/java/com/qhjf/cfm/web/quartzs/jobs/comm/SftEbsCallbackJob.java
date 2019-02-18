ba151016ad92b0a7619dc577577743b85c1ca5db
hongda <hongda@qihangjinfu.com>
2019-02-18 16:35:46 +0800

    通feature-ddh2 a3fa5e3a08880cb75a7643896d609b894ab3d91a 同步

--- cfm_quartzs_jobs/src/main/java/com/qhjf/cfm/web/quartzs/jobs/comm/SftEbsCallbackJob.java
+++ cfm_quartzs_jobs/src/main/java/com/qhjf/cfm/web/quartzs/jobs/comm/SftEbsCallbackJob.java
@@ -11,12 +11,14 @@ import com.jfinal.plugin.activerecord.Record;
 import com.qhjf.cfm.web.constant.WebConstant;
 import com.qhjf.cfm.web.webservice.la.LaCallback;
 import com.qhjf.cfm.web.webservice.sft.SftCallBack;
-
+/**
+ *对完结的原始数据回调EBS失败的情况，再次回调EBS
+ */
 public class SftEbsCallbackJob implements Job{
 
 	@Override
 	public void execute(JobExecutionContext context) throws JobExecutionException {
-		List<Record> list = Db.find(Db.getSql("ebs_callback_cfm.getEBSOriginList"), WebConstant.SftInterfaceStatus.SFT_INTER_PROCESS_F.getKey(),WebConstant.SftInterfaceStatus.SFT_INTER_PROCESS_S.getKey(),WebConstant.SftCallbackStatus.SFT_CALLBACK_F.getKey(),WebConstant.SftCallbackStatus.SFT_NO_CALLBACK.getKey());
+		List<Record> list = Db.find(Db.getSql("ebs_cfm.getEBSCallBackOriginList"), WebConstant.SftInterfaceStatus.SFT_INTER_PROCESS_F.getKey(),WebConstant.SftInterfaceStatus.SFT_INTER_PROCESS_S.getKey(),WebConstant.SftCallbackStatus.SFT_CALLBACK_F.getKey(),WebConstant.SftCallbackStatus.SFT_NO_CALLBACK.getKey());
 		if(list != null) {
 			for(Record record : list) {
 				SftCallBack callback = new SftCallBack();
--- cfm_quartzs_jobs/src/main/java/com/qhjf/cfm/web/quartzs/jobs/comm/SftLaCallbackJob.java
+++ cfm_quartzs_jobs/src/main/java/com/qhjf/cfm/web/quartzs/jobs/comm/SftLaCallbackJob.java
@@ -11,12 +11,14 @@ import com.jfinal.plugin.activerecord.Record;
 import com.qhjf.cfm.web.constant.WebConstant;
 import com.qhjf.cfm.web.webservice.la.LaCallback;
 import com.qhjf.cfm.web.webservice.sft.SftCallBack;
-
+/**
+ *对完结的原始数据回调LA失败的情况，再次回调LA
+ */
 public class SftLaCallbackJob implements Job{
 
 	@Override
 	public void execute(JobExecutionContext context) throws JobExecutionException {
-		List<Record> list = Db.find(Db.getSql("la_callback_cfm.getLAOriginList"), WebConstant.SftInterfaceStatus.SFT_INTER_PROCESS_F.getKey(),WebConstant.SftInterfaceStatus.SFT_INTER_PROCESS_S.getKey(),WebConstant.SftCallbackStatus.SFT_CALLBACK_F.getKey(),WebConstant.SftCallbackStatus.SFT_NO_CALLBACK.getKey());
+		List<Record> list = Db.find(Db.getSql("la_cfm.getLACallBackOriginList"), WebConstant.SftInterfaceStatus.SFT_INTER_PROCESS_F.getKey(),WebConstant.SftInterfaceStatus.SFT_INTER_PROCESS_S.getKey(),WebConstant.SftCallbackStatus.SFT_CALLBACK_F.getKey(),WebConstant.SftCallbackStatus.SFT_NO_CALLBACK.getKey());
 		if(list != null){
 			for(Record record : list){
 				SftCallBack callback = new SftCallBack();
