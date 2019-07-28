f1348a1456b64ee565ca58e4106710cf8a3a6f3c
phWangK <wangpanhang009@sina.com>
2019-07-28 23:05:50 +0800

    回退版本DbtOptypeMgr.java文件

--- cfm_module_dbt/src/main/java/com/qhjf/cfm/web/DbtOptypeMgr.java
+++ cfm_module_dbt/src/main/java/com/qhjf/cfm/web/DbtOptypeMgr.java
@@ -8,7 +8,6 @@ import com.qhjf.cfm.web.validates.RequiredParamsValidate;
  * @Date: 2018/8/9
  * @Description:
  */
-//测试git可否正常提交代码07282019day21
 public class DbtOptypeMgr extends AbstractOptypeMgr {
     @Override
     public void registe() {
