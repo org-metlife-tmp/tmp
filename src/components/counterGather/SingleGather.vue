aa249bdf86d3ffdf12711a51952c47fc39f27b70
maojiu <maojiu52100>
2019-09-25 11:28:10 +0800

    柜面收前端bug修复_个单详情

--- src/components/counterGather/SingleGather.vue
+++ src/components/counterGather/SingleGather.vue
@@ -778,7 +778,7 @@
                     </el-row>
                 </el-form>
                 <span slot="footer" class="dialog-footer">
-                <el-button type="warning" size="mini" plain @click="dialogVisible = false" disabled>取 消</el-button>
+                <el-button type="warning" size="mini" plain @click="dialogVisibledetail = false" >取 消</el-button>
                 <el-button type="warning" size="mini" @click="saveData"  disabled>确 定</el-button>
             </span>
             </el-dialog>
